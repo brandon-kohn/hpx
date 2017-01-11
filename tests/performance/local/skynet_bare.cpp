8f42b47837665dc669d77e724d7bed22117896a0
Brandon Kohn <blkohn@hotmail.com>
2017-01-11 14:30:09 -0500

    Made number of actors to spawn a command line option.

--- tests/performance/local/skynet_bare.cpp
+++ tests/performance/local/skynet_bare.cpp
@@ -3,29 +3,31 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#include <hpx/hpx_main.hpp>
+#include <hpx/hpx_init.hpp>
 #include <hpx/hpx.hpp>
 #include <hpx/lcos/channel.hpp>
 #include <hpx/runtime/threads/thread.hpp>
 #include <hpx/include/iostreams.hpp>
 
-#include <cstdint>
-
-using channel_t = hpx::lcos::local::channel<std::int64_t>;
+using channel_t = hpx::lcos::local::channel<std::uint64_t>;
 
 ///////////////////////////////////////////////////////////////////////////////
-void skynet_bare(channel_t& c, std::int64_t num, std::int64_t size, std::int64_t div)
+void skynet_bare(channel_t& c, std::uint64_t num, std::uint64_t size, std::uint64_t div)
 {
     if (size != 1)
     {
 		channel_t rc;
-		for (std::int64_t i = 0; i < div; ++i) 
-		{
-			auto sub_num = num + i * size / div;
-			hpx::thread{ [&rc, sub_num, size, div]() { skynet_bare(rc, sub_num, size / div, div); } }.detach();
+		for (std::uint64_t i = 0; i < div; ++i) 
+		{			
+			hpx::thread{ [&rc, num, i, size, div]() 
+			{
+				auto sd = size / div;
+				auto sub_num = num + i * sd;
+				skynet_bare(rc, sub_num, sd, div); 
+			} }.detach();
 		}
-		std::int64_t sum{ 0 };
-		for (std::int64_t i = 0; i < div; ++i) 
+		std::uint64_t sum{ 0 };
+		for (std::uint64_t i = 0; i < div; ++i) 
 			sum += rc.get().get();
 		c.set(sum);
 	}
@@ -34,13 +36,27 @@ void skynet_bare(channel_t& c, std::int64_t num, std::int64_t size, std::int64_t
 }
 
 ///////////////////////////////////////////////////////////////////////////////
-int main()
+int hpx_main(boost::program_options::variables_map& vm)
 {
+	std::uint64_t n_actors = vm["n-actors"].as<std::uint64_t>();
 	std::uint64_t t = hpx::util::high_resolution_clock::now();
 	channel_t rc;
-	skynet_bare(rc, 0, 100000, 10);
+	skynet_bare(rc, 0, n_actors, 10);
 	t = hpx::util::high_resolution_clock::now() - t;
 	hpx::cout << rc.get().get() << " in " << (t / 1e6) << " ms.\n";
-    return 0;
+
+	return hpx::finalize();
 }
 
+int main(int argc, char* argv[])
+{
+	using namespace boost::program_options;
+	options_description desc_commandline(
+		"Usage: " HPX_APPLICATION_STRING " [options]");
+
+	desc_commandline.add_options()
+		("n-actors,N", value<std::uint64_t>()->default_value(100000),
+			"number of actors to spawn in total");
+
+	return hpx::init(desc_commandline, argc, argv);
+}
